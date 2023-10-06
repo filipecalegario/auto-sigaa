0f334f75b61c7343dd495d747298646966a2bc8b
filipecalegario <fcac@cin.ufpe.br>
2023-10-06 12:28:28 -0300

    add specific error handling

--- auto_sigaa.py
+++ auto_sigaa.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from sys import exit
 
 # Carregue a planilha
 df = pd.read_excel("template.xlsx")
@@ -28,6 +30,8 @@ for index, row in df.iterrows():
         td_aluno = driver.find_element(By.XPATH, f"//td[contains(text(), '{estudante}')]")
         print(f"achou: {td_aluno.text}")
 
+        # A duração é o tempo que o Selenium irá esperar para interagir com o elemento
+        # A unidade aqui é segundos. Ex: 0.5 = 500ms = meio segundo
         duration = 0.5
 
         # A partir do TD, navegue até o TR pai e depois localize o input de nota que está no próximo TD
@@ -37,8 +41,14 @@ for index, row in df.iterrows():
         # É preciso multiplicar por 10, por conta da entrada de dados do SIGAA que já adicionar a vírgula automaticamente
         input_nota.send_keys(str(nota*10)) 
         time.sleep(duration)
+    except KeyError:
+        print("Erro ao tentar achar a coluna NOME ou NOTA. Verifique se você tem essas colunas em sua planilha.")
+        exit()
+    except NoSuchElementException:
+        print(f"Erro ao tentar achar o nome de {estudante} ou campo de nota. Verifique se você está na página correta e se o nome do estudante na planilha está com o mesmo nome do SIGAA.")
     except:
-        print(f"Erro ao tentar achar: {estudante}")
+        print("Erro desconhecido. Verifique se você está na página correta.")
+        exit()
 
 print("Script finalizado com sucesso!")
 print("Verifique se as notas foram lançadas corretamente.")

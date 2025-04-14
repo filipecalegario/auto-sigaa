74fa77882bc120aaeecb5c930356e323b2413aad
filipecalegario <fcac@cin.ufpe.br>
2025-04-14 16:30:57 -0300

    update Excel file references and enhance note input handling in the script

--- auto_sigaa.py
+++ auto_sigaa.py
@@ -6,7 +6,7 @@ from selenium.common.exceptions import NoSuchElementException
 from sys import exit
 
 # Carregue a planilha
-df = pd.read_excel("template.xlsx")
+df = pd.read_excel("2024_2_if866.xlsx")
 print('Planilha carregada com sucesso!')
 print(df.dtypes)
 
@@ -24,7 +24,9 @@ for index, row in df.iterrows():
     try: 
         # Supondo que você tem uma coluna "NOME" e uma coluna "NOTA" em sua planilha
         estudante = row["NOME"]
-        nota = row["NOTA"]
+        nota_AE = row["AE"]
+        nota_PA = row["PA"]
+        nota_PF = row["PF"]
 
         # Localize o elemento TD que contém o nome atual da lista de estudantes
         td_aluno = driver.find_element(By.XPATH, f"//td[contains(text(), '{estudante}')]")
@@ -33,13 +35,23 @@ for index, row in df.iterrows():
         # A duração é o tempo que o Selenium irá esperar para interagir com o elemento
         # A unidade aqui é segundos. Ex: 0.5 = 500ms = meio segundo
         duration = 0.5
+        
+        td_nota_AE = td_aluno.find_element(By.XPATH, "./following-sibling::td")
+        td_nota_PA = td_nota_AE.find_element(By.XPATH, "./following-sibling::td")
+        td_nota_PF = td_nota_PA.find_element(By.XPATH, "./following-sibling::td")
 
         # A partir do TD, navegue até o TR pai e depois localize o input de nota que está no próximo TD
-        input_nota = td_aluno.find_element(By.XPATH, "./following-sibling::td/input")
+        input_nota_AE = td_aluno.find_element(By.XPATH, "./following-sibling::td/input")
+        input_nota_PA = td_nota_AE.find_element(By.XPATH, "./following-sibling::td/input")
+        input_nota_PF = td_nota_PA.find_element(By.XPATH, "./following-sibling::td/input")
 
         # Agora, o Selenium irá interagir com o input para preencher a nota
         # É preciso multiplicar por 10, por conta da entrada de dados do SIGAA que já adicionar a vírgula automaticamente
-        input_nota.send_keys(str(nota*10)) 
+        input_nota_AE.send_keys(str(nota_AE*10)) 
+        time.sleep(duration)
+        input_nota_PA.send_keys(str(nota_PA*10)) 
+        time.sleep(duration)
+        input_nota_PF.send_keys(str(nota_PF*10)) 
         time.sleep(duration)
     except KeyError:
         print("Erro ao tentar achar a coluna NOME ou NOTA. Verifique se você tem essas colunas em sua planilha.")
